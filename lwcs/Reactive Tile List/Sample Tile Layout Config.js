854c9469f74f41c229b7da40d78ece6b4ff78d0d
Rebbe Pod <66928914+RebbePod@users.noreply.github.com>
2025-06-06 12:10:12 -0400

    Update Sample Tile Layout Config.js

--- lwcs/Reactive Tile List/Sample Tile Layout Config.js
+++ lwcs/Reactive Tile List/Sample Tile Layout Config.js
@@ -1,82 +1,82 @@
 {
     "columns": [
-      {
-        "rows": [
-          {
-            "columns": [
-              {
-                "type": "field",
-                "value": "Name",
-                "showLabel": true,
-                "style": "white-space: nowrap;"
-              },
-              {
-                "type": "text",
-                "value": " - VIP"
-              }
-            ],
-            "alignment": {
-              "h": "center"
-            }
-          },
-          {
-            "columns": [
-              {
-                "type": "text",
-                "value": "applied"
-              }
-            ],
-            "alignment": {
-              "h": "right"
-            }
-          },
-          {
-            "columns": [
-              {
-                "type": "field",
-                "value": "Email",
-                "showLabel": true
-              },
-              {
-                "type": "text",
-                "value": "Centered Text"
-              },
-              {
-                "type": "field",
-                "value": "Level__c",
-                "showLabel": true
-              }
-            ],
-            "alignment": [
-              {
-                "h": "left"
-              },
-              {
-                "h": "center"
-              },
-              {
-                "h": "right"
-              }
+        {
+            "rows": [
+                {
+                    "columns": [
+                        {
+                            "type": "field",
+                            "value": "Name",
+                            "showLabel": true,
+                            "style": "white-space: nowrap;"
+                        },
+                        {
+                            "type": "text",
+                            "value": " - VIP"
+                        }
+                    ],
+                    "alignment": {
+                        "h": "center"
+                    }
+                },
+                {
+                    "columns": [
+                        {
+                            "type": "text",
+                            "value": "applied"
+                        }
+                    ],
+                    "alignment": {
+                        "h": "right"
+                    }
+                },
+                {
+                    "columns": [
+                        {
+                            "type": "field",
+                            "value": "Email",
+                            "showLabel": true
+                        },
+                        {
+                            "type": "text",
+                            "value": "Centered Text"
+                        },
+                        {
+                            "type": "field",
+                            "value": "Level__c",
+                            "showLabel": true
+                        }
+                    ],
+                    "alignment": [
+                        {
+                            "h": "left"
+                        },
+                        {
+                            "h": "center"
+                        },
+                        {
+                            "h": "right"
+                        }
+                    ]
+                },
+                {
+                    "columns": [
+                        {
+                            "type": "field",
+                            "value": "Some_Text__c",
+                            "showLabel": true,
+                            "style": "color: darkblue; font-weight: bold; font-size: 1.1rem;",
+                            "labelStyle": "font-weight: normal;"
+                        }
+                    ],
+                    "alignment": {
+                        "h": "left"
+                    }
+                }
             ]
-          },
-          {
-            "columns": [
-              {
-                "type": "field",
-                "value": "Some_Text__c",
-                "showLabel": true,
-                "style": "color: darkblue; font-weight: bold; font-size: 1.1rem;",
-                "labelStyle": "font-weight: normal;"
-              }
-            ],
-            "alignment": {
-              "h": "left"
-            }
-          }
-        ]
-      }
+        }
     ],
-    "alignment": {
-      "v": "middle"
+        "alignment": {
+        "v": "middle"
     }
-  }
+}
